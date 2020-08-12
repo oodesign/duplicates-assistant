4268292aefba99846cfd8ae7dc59b6017ca6c1c4
oodesign <oscar@oodesign.me>
2020-08-12 12:37:39 +0200

    Initial commit

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,11 +3,10 @@ import { testAssistant } from '@sketch-hq/sketch-assistant-utils'
 
 import Assistant from '..'
 
-test('test assistant', async () => {
-  const { violations, ruleErrors } = await testAssistant(
+test('test duplicate symbols', async () => {
+  const { ruleErrors } = await testAssistant(
     resolve(__dirname, './empty.sketch'),
     Assistant,
   )
-  expect(violations[0].message).toBe('Hello world')
   expect(ruleErrors).toHaveLength(0)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,42 @@
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
+const duplicateSymbols: RuleDefinition = {
   rule: async (context) => {
-    context.utils.report('Hello world')
+    interface Duplicate {
+      name: string;
+      number: number;
+    }
+    var allSymbols = [];
+    var duplicates: Array<Duplicate> = [];
+    var totalDuplicates = 1;
+    for (const symbol of context.utils.objects.symbolMaster) {
+      if (allSymbols.indexOf(symbol.name) >= 0) {
+        totalDuplicates++;
+        var existingElement = duplicates.find(element => element.name == symbol.name);
+        if (existingElement != null)
+          existingElement.number++;
+        else
+          duplicates.push({ "name": symbol.name, "number": 2 })
+      }
+      else
+        allSymbols.push(symbol.name);
+    }
+
+    if (duplicates.length > 0)
+      context.utils.report("There " + (totalDuplicates > 1 ? "are " : "is ") + totalDuplicates + " duplicate symbols.");
   },
-  name: 'sketch-assistant-template/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+  name: 'oodesign-duplicates-assistant/duplicate-symbols',
+  title: 'Duplicate symbols',
+  description: 'Reports duplicate symbols in your design file.',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorld],
+    name: 'oodesign-duplicates-assistant',
+    rules: [duplicateSymbols],
     config: {
       rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+        'oodesign-duplicates-assistant/duplicate-symbols': { active: true },
       },
     },
   }
